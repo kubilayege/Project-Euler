d4b6d437aff3fd384f0c2464dda3f1a629f6772c
Kubilay Ege <Kubilay Ege>
2016-04-26 17:22:47 +0300

    001

---
+++ Problem 1/problem1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() 
+{
+	int j,sum=0;
+	for( j=1 ; j<1000 ; j++)
+	{
+		if( j%3==0 || j%5==0 )
+		{
+			sum = sum + j;
+		}
+	}
+	printf("%d\n",sum);
+	return 0;
+}
